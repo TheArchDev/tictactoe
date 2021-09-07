db98d14d96ea2fd5a80877d2b3509aeb8f31b1fa
Alex Archer <alex@touchsurgery.com>
2021-09-07 14:35:02 +0100

    Updated version, including checks for winning move and final move

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+NOUGHT = "O"
+CROSS = "X"
+
 class Player(object):
-	def __init__(self, name):
+	def __init__(self, name, mark):
 		self.name = name
+		self.mark = mark
 
 	def __str__(self):
 		return self.name
 
 class Game(object):
 
-	NOUGHT = "O"
-	CROSS = "X"
+	SPACE = "_"
 	WINNING_SCENARIOS = (
 		((0, 0), (0, 1), (0, 2)),
 		((1, 0), (1, 1), (1, 2)),
@@ -21,54 +24,107 @@ class Game(object):
 	)
 
 	def __init__(self, player_one, player_two):
-		self.board = [
-			["_", "_", "_"],
-			["_", "_", "_"],
-			["_", "_", "_"]
+		self.board = self.new_board()
+		self.current_player = player_one
+		self.waiting_player = player_two
+		self.winner = None
+
+	def new_board(self):
+		return [
+			[self.SPACE, self.SPACE, self.SPACE],
+			[self.SPACE, self.SPACE, self.SPACE],
+			[self.SPACE, self.SPACE, self.SPACE]
 		]
-		self.player_one = player_one
-		self.player_two = player_two
-		self.mark_types = {self.player_one: self.NOUGHT, self.player_two: self.CROSS}
-		self.finished = False
-
-	def check_for_winner(self, player):
-		scenario_results = []
-		for scenario in self.WINNING_SCENARIOS:
-			scenario_result = all([self.board[x][y] == self.mark_types[player] for x, y in scenario])
-			scenario_results.append(scenario_result)
-
-		return any(scenario_results)
-
-	def make_play(self, player, x_coord, y_coord):
-		mark_type = self.mark_types[player]
-		self.board[x_coord][y_coord] = mark_type
-		print(self)
-		if self.check_for_winner(player):
-			self.finished = True
-			print(f'{player} wins!')
 
 	def __str__(self):
-		return '\n'.join([' '.join(row) for row in self.board])
+		return "\n".join([" ".join(row) for row in self.board])
+
+	def visual_y_location(self, y_coordinate):
+		return 2-y_coordinate
+
+	def valid_play(self, x_coordinate, y_coordinate):
+		return self.board[self.visual_y_location(y_coordinate)][x_coordinate] == self.SPACE
+
+	def play_turn(self, x_coordinate, y_coordinate):
+		self.board[self.visual_y_location(y_coordinate)][x_coordinate] = self.current_player.mark
+		self.board
+		if self.is_winning_turn():
+			self.winner = True
+		elif self.is_final_turn():
+			self.winner = False
+		else:
+			self.current_player, self.waiting_player = self.waiting_player, self.current_player
+
+	def is_winning_turn(self):
+		scenario_matches = []
+		for scenario in self.WINNING_SCENARIOS:
+			scenario_match = all([self.board[self.visual_y_location(y)][x] == self.current_player.mark for x, y in scenario])
+			scenario_matches.append(scenario_match)
+
+		return any(scenario_matches)
+
+	def is_final_turn(self):
+		return not any([self.SPACE in row for row in self.board])
+
+
+def valid_coordinate(value):
+	valid = False
+	try:
+		coord = int(value)
+		valid = coord in range(3)
+	except ValueError:
+		pass
+	return valid
+
+
+def prompt_for_y_coordinate():
+	return prompt_for_coordinate("y")
+
+
+def prompt_for_x_coordinate():
+	return prompt_for_coordinate("x")
+
+
+def prompt_for_coordinate(axis):
+	return input(f"Enter {axis} coordinate (0, 1 or 2)... ").strip()
+
+
+def prompt_for_coordinates():
+	x_coordinate = prompt_for_x_coordinate()
+	while not valid_coordinate(x_coordinate):
+		x_coordinate = prompt_for_x_coordinate()
+
+	y_coordinate = prompt_for_y_coordinate()
+	while not valid_coordinate(y_coordinate):
+		y_coordinate = prompt_for_y_coordinate()
+
+	return int(x_coordinate), int(y_coordinate)
 
 
 def main():
 	print("Welcome to this game of Tic-Tac-Toe!")
-	current_player = player_one = Player(input("Name of player one: ").strip())
-	next_player = player_two = Player(input("Name of player two: ").strip())
+	player_one = Player(input("Name of player one: ").strip(), NOUGHT)
+	player_two = Player(input("Name of player two: ").strip(), CROSS)
+	
 	game = Game(player_one, player_two)
-	print(game)
-	while not game.finished:
-		print(f"\n{current_player} to play...")
-		x_coord = int(input("x coordinate? ").strip())
-		y_coord = int(input("y coordinate? ").strip())
-		game.make_play(current_player, x_coord, y_coord)
-		current_player, next_player = next_player, current_player
+	
+	while game.winner is None:
+		print(f"\n{game.current_player} to play...")
+		print(game)
 
+		x_coordinate, y_coordinate = prompt_for_coordinates()
+		while not game.valid_play(x_coordinate, y_coordinate):
+			print(f"({x_coordinate}, {y_coordinate}) spot already taken!")
+			x_coordinate, y_coordinate = prompt_for_coordinates()
+		
+		game.play_turn(x_coordinate, y_coordinate)
+
+	print(game)
+	if game.winner is True:
+		print(f"\n{game.current_player} wins!")
+	elif game.winner is False:
+		print("\nNo spaces left - the game is a draw!")
+	print("Thanks for playing!")
 
 if __name__ == "__main__":
 	main()
-
-# no more possible moves; ie board is full
-# sort out coordinates
-# don't allow an already-chosen location
-# handle out of range coordinates
